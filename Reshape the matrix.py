889eda196ce0efde984b0133bb5c146b91e74f66
ritasol <95837544+ritasol@users.noreply.github.com>
2023-01-20 09:54:16 -0900

    python

--- Reshape the matrix.py
+++ Reshape the matrix.py
@@ -1,14 +1,15 @@
-class Solution:
-    def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]: 
-        ans = []
-        container = []
-        for i in range(len(mat)):
-            for j in range(len(mat[i])):
-                container.append(mat[i][j])
-        
-        if len(container) != r*c:
-            return mat
-        else:
-            for i in range(r):
-                ans.append(container[i*c:(i*c)+c])
-            return ans
+nums =  [[1,2],[3,4]]
+r = 2
+c = 4
+new_ans = []
+container = []
+for i in range(len(nums)):
+    for j in range(len(nums[i])):
+        container.append(nums[i][j])
+print(container)
+if len(container) != r*c:
+    print(nums)
+else:
+    for i in range(r):
+        new_ans.append(container[i*c:(i*c)+c])
+    print(new_ans)
---
+++ Toeplitz matrix.py
@@ -0,0 +1,5 @@
+mat = [[1,2,3,4],[5,1,2,3],[9,5,1,2]]
+lst = []
+for i in range(len(mat)):
+    lst.append(mat[i][i])
+print(lst)
